f9adb8f399412481799a44da763b654b09aa270f
tim.stannard <tim.stannard@yoobeecolleges.com>
2022-05-30 11:56:56 +1200

    replaced token

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoidGltc3Rhbm5hcmQ5OSIsImEiOiJjbDJ2Y3l5eG4wYXQ1M2pvYXQ0enV1djZjIn0.-5tfA6QNFW8egI8KDX_Twg';
+mapboxgl.accessToken = '<enter your token here>';
 
 const map = new mapboxgl.Map({
     container: 'map', // container ID
